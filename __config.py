115c3bb995542bf5fdeb2ddc296181d1bae5d38e
PanXXHH <987384390@qq.com>
2024-08-09 15:54:14 +0800

    Update .gitignore

--- __config.py
+++
@@ -1,7 +0,0 @@
-
-
-TARGET_PATHS = [r"G:\Program Data\ONEDRIVE\987384390\OneDrive",
-                r"F:\杂乱文件",
-                r"G:\Program Data\ONEDRIVE2023\MY\OneDrive - PANSCLOUD",
-                r"G:\Program Data\ONEDRIVE2023\LEARNING\OneDrive - PANSCLOUD",
-                r"G:\Program Data\ONEDRIVE\TB0001\OneDrive - MsTeams\DATA2 - TTP - DATA"]
